473e877368f141466329e4884bd6102ed3b7b117
Sofía Múnera <somuneram@unal.edu.co>
2021-04-01 17:10:45 +0000

    Creacion del archivo

---
+++ main.py
@@ -0,0 +1,12 @@
+# MANEJO ARCHIVOS DE TEXTO
+
+# para abrir un archivo usamos el metodo open(nombre archivo, argumento), si este no existe, se crea
+
+# argumento puede ser r=lectura w=escritura a=escritura al final del archibo +=para leer y escribir
+
+# Archivos van .txt
+
+f=open('Hoja1.txt','w')
+
+  # creamos el archivo y podemos escribir en el 
+
